d73127471280bdc08bc8282bf4eff5aeae5a7152
RamaSandhy <rshandy07@gmail.com>
2025-05-08 03:29:09 +0700

    menambahkan halaman TodoPage dengan form input dan pemilihan tanggal

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -4,4 +4,10 @@ import '../bloc/todo_bloc.dart';
 import '../bloc/todo_event.dart';
 import '../bloc/todo_state.dart';
 
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
